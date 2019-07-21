eac9a6ad9790a7e099e750657dcebab06fa232dd
System Administrator <root@Huus-iMac.local>
2019-07-21 17:16:58 +0900

    Updates

---
+++ precache-manifest.dcfe629e16e033e5f6f3b38b2761fc87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16d174051dd39147214a146164366ae",
+    "url": "/Othello/index.html"
+  },
+  {
+    "revision": "1211b8a9c72a5f3a3a0e",
+    "url": "/Othello/static/css/main.4135f8a4.chunk.css"
+  },
+  {
+    "revision": "8f5a58e67193d441e3c6",
+    "url": "/Othello/static/js/2.2dc22022.chunk.js"
+  },
+  {
+    "revision": "1211b8a9c72a5f3a3a0e",
+    "url": "/Othello/static/js/main.9b00e93e.chunk.js"
+  },
+  {
+    "revision": "e53834819ef503975a99",
+    "url": "/Othello/static/js/runtime~main.951e2ceb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Othello/static/media/logo.5d5d9eef.svg"
+  }
+]);
