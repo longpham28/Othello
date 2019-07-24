c3a0a7091f17c5c28acd55b405fd51f88d8220b1
Long <huulongpham28@gmail.com>
2019-07-24 09:14:35 +0900

    Updates

--- precache-manifest.2622b88d60cc5ccca5dd5e15fa946b7f.js
+++ precache-manifest.2622b88d60cc5ccca5dd5e15fa946b7f.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5fbe55c6cb9929eccd16dae8a85978d0",
+    "revision": "106ecdd89dead8725adaa8bd759aaedb",
     "url": "/Othello/index.html"
   },
   {
-    "revision": "d6423758694574383190",
-    "url": "/Othello/static/css/main.5734d555.chunk.css"
+    "revision": "61c76ec881f70b8fbce8",
+    "url": "/Othello/static/css/main.053c4af9.chunk.css"
   },
   {
-    "revision": "0f2b6dd91fc8ffd26883",
-    "url": "/Othello/static/js/2.b3bb5761.chunk.js"
+    "revision": "5495893e2e3b76bd2c49",
+    "url": "/Othello/static/js/2.86aeb82a.chunk.js"
   },
   {
-    "revision": "d6423758694574383190",
-    "url": "/Othello/static/js/main.66639ed4.chunk.js"
+    "revision": "61c76ec881f70b8fbce8",
+    "url": "/Othello/static/js/main.ba8d38a8.chunk.js"
   },
   {
     "revision": "e53834819ef503975a99",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Othello/precache-manifest.a37ccf88f6dd7429f207ab7bea29e9a3.js"
+  "/Othello/precache-manifest.2622b88d60cc5ccca5dd5e15fa946b7f.js"
 );
 
 self.addEventListener('message', (event) => {
